fe41f2eb722752ce78e8a81b006fc2e57d92e917
Kiran Vadakkath <kiranvadakath@gmail.com>
2018-09-05 11:11:01 -0400

    talk map

--- talkmap/org-locations.js
+++ talkmap/org-locations.js
@@ -1,22 +1,17 @@
 var addressPoints = [
   [
-    "Berkeley CA, USA",
-    37.8708393,
-    -122.2728638
+    "MES COllege of Engineering, Kerala, INDIA",
+    10.8295693,
+    76.0206069
   ],
   [
-    "London, UK",
-    51.5073219,
-    -0.1276473
+    "MES COllege of Engineering, Kerala, INDIA",
+    10.8295693,
+    76.0206069
   ],
   [
-    "San Francisco, California",
-    37.7792808,
-    -122.4192362
-  ],
-  [
-    "Los Angeles, CA",
-    34.0543942,
-    -118.2439408
+    "Frazer Town, Bangalore, India",
+    12.9545163,
+    77.350044
   ]
 ];
